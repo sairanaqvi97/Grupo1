42e8aac0fe3d5be2d7669e8e5e5676b73e66e1b0
JuliaT <julia.trejo@bootcamp-upgrade.com>
2024-07-12 00:54:22 +0200

    primer header del proyecto

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+function App() {
+
+  return (<body>
+   <Header></Header>
+   </body>
+  )
+}
+
+export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '../styles/Menu.module.css';
+
+const Menu = () => {
+    return (
+        <nav>
+            <ul className={styles.menu}>
+                <li>
+                    <Link href="/">Inicio</Link>
+                </li>
+                <li>
+                    <Link href="/products">Products</Link>
+                </li>
+                <li>
+                    <Link href="/profile">Profile</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+export default Menu
