904848b6a1b6afb424d65a31d32449122228ee72
JuliaT <julia.trejo@bootcamp-upgrade.com>
2024-07-26 02:12:51 +0200

    card

--- src/components/MeetupCard/MeetupCard.jsx
+++ src/components/MeetupCard/MeetupCard.jsx
@@ -20,65 +20,59 @@ function MeetupCard() {
 
   return (
     <div id="events" className="card-container">
-      <h1 className="textXL">MeetNow</h1>
-      <h2 className="tubo">¡Quedadas por un tubo!</h2>
-      <ul>
-        {meetups.map((meetup) => {
-          const keywords = Array.isArray(meetup.keywords)
-            ? meetup.keywords
-            : [];
+      {meetups.map((meetup) => {
+        const keywords = Array.isArray(meetup.keywords) ? meetup.keywords : [];
 
-          return (
-            <li key={meetup.id} className="card">
-              <h1 className="card-title">{meetup.title}</h1>
-              <div className="card-paragraph">
-                <p>
-                  <strong>Host:</strong> {meetup.host.name}
-                </p>
-                <p>
-                  <strong>Contact:</strong> {meetup.host.contact}
-                </p>
-                <div className="image-wrapper">
-                  <img
-                    className="wrapped-image"
-                    src={meetup.host.avatar}
-                    alt={`${meetup.host.name}'s avatar`}
-                    width="50"
-                    height="50"
-                  />
-                </div>
-                <p>
-                  <strong>Location:</strong> {meetup.googleMapsLink}
-                </p>
-                <p>
-                  <a
-                    href={meetup.googleMapsLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    View on Google Maps
-                  </a>
-                </p>
-                <p>
-                  <strong>Type:</strong> {meetup.type}
-                </p>
-                <p>
-                  <strong>Keywords:</strong> {keywords.join(", ")}
-                </p>
-                <p>
-                  <strong>Description:</strong> {meetup.description}
-                </p>
-                <p>
-                  <strong>Max Attendees:</strong> {meetup.maxAttendees}
-                </p>
-                <p>
-                  <strong>Active:</strong> {meetup.activeMeetup ? "Yes" : "No"}
-                </p>
+        return (
+          <div key={meetup.id} className="card">
+            <h1 className="card-title">{meetup.title}</h1>
+            <div className="card-paragraph">
+              <p>
+                <strong>Host:</strong> {meetup.host.name}
+              </p>
+              <p>
+                <strong>Contact:</strong> {meetup.host.contact}
+              </p>
+              <div className="image-wrapper">
+                <img
+                  className="wrapped-image"
+                  src={meetup.host.avatar}
+                  alt={`${meetup.host.name}'s avatar`}
+                  width="50"
+                  height="50"
+                />
               </div>
-            </li>
-          );
-        })}
-      </ul>
+              <p>
+                <strong>Location:</strong> {meetup.googleMapsLink}
+              </p>
+              <p>
+                <a
+                  href={meetup.googleMapsLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View on Google Maps
+                </a>
+              </p>
+              <p>
+                <strong>Type:</strong> {meetup.type}
+              </p>
+              <p>
+                <strong>Keywords:</strong> {keywords.join(", ")}
+              </p>
+              <p>
+                <strong>Description:</strong> {meetup.description}
+              </p>
+              <p>
+                <strong>Max Attendees:</strong> {meetup.maxAttendees}
+              </p>
+              <p>
+                <strong>Active:</strong> {meetup.activeMeetup ? "Yes" : "No"}
+              </p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -3,18 +3,16 @@ import Header from "../components/Header/Header";
 import { getMeetups } from "../components/service/MeetupService";
 
 export const Perfil = () => {
+  const [userMeetups, setUserMeetups] = useState([]);
 
-const [userMeetups, setUserMeetups] = useState([]);
+  useEffect(() => {
+    getMeetups()
+      .then((data) => setUserMeetups(data))
+      .catch((error) => console.error("Error al recibir la informacion"));
+  }, []);
 
-useEffect(() => {
-  getMeetups()
-  .then(data => setUserMeetups(data))
-  .catch(error => console.error('Error al recibir la informacion'));
-}, []);
-
-const activeMeetups = userMeetups.filter(meetup => meetup.activeMeetup);
-const pastMeetups = userMeetups.filter(meetup => !meetup.activeMeetup);
-
+  const activeMeetups = userMeetups.filter((meetup) => meetup.activeMeetup);
+  const pastMeetups = userMeetups.filter((meetup) => !meetup.activeMeetup);
 
   return (
     <div>
@@ -22,29 +20,85 @@ const pastMeetups = userMeetups.filter(meetup => !meetup.activeMeetup);
       <h1>Bienvenido a tu perfil</h1>
       <h2>Despliega para ver los ajustes de tu perfil</h2>
 
-<section>
-  <h2>Mis quedadas activas</h2>
-  <ul>
-    {activeMeetups.map(meetup => (
-      <li key={meetup.id}>
-        <h3>{meetup.title}</h3>
-        <img src={meetup.host.avatar} alt={`${meetup.host.name}'s avatar`} width="50" height="50" />
-        <p><strong>Type:</strong> {meetup.type}</p>
-            <p><strong>Keywords:</strong> {meetup.keywords.join(', ')}</p>
-            <p><strong>Description:</strong> {meetup.description}</p>
-            <p><strong>Max Attendees:</strong> {meetup.maxAttendees}</p>
-            <p><strong>Active:</strong> {meetup.activeMeetup ? 'Yes' : 'No'}</p>
+      <form className="profile-form">
+        <div className="form-group">
+          <label htmlFor="profileImage">Subir Imagen:</label>
+          <input
+            type="file"
+            id="profileImage"
+            name="profileImage"
+            accept="image/*"
+          ></input>
+        </div>
 
-      </li>
-    ))}
-    
-  </ul>
-</section>
+        <div className="form-group">
+          <label htmlFor="firstName">Nombre:</label>
+          <input type="text" id="firstName" name="firstName" required></input>
+        </div>
 
-<section>
-  <h2>Mis quedadas pasadas</h2>
-  <ul>
-          {pastMeetups.map(meetup => (
+        <div className="form-group">
+          <label htmlFor="lastName">Apellidos:</label>
+          <input type="text" id="lastName" name="lastName" required></input>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="age">Edad:</label>
+          <input type="number" id="age" name="age" required></input>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="gender">Sexo:</label>
+          <select id="gender" name="gender" required>
+            <option value="male">Masculino</option>
+            <option value="female">Femenino</option>
+            <option value="other">Otro</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="birthdate">Fecha de Nacimiento:</label>
+          <input type="date" id="birthdate" name="birthdate" required></input>
+        </div>
+
+        <button type="submit">Guardar Cambios</button>
+      </form>
+
+      <section>
+        <h2>Mis quedadas activas</h2>
+        <ul>
+          {activeMeetups.map((meetup) => (
+            <li key={meetup.id}>
+              <h3>{meetup.title}</h3>
+              <img
+                src={meetup.host.avatar}
+                alt={`${meetup.host.name}'s avatar`}
+                width="50"
+                height="50"
+              />
+              <p>
+                <strong>Type:</strong> {meetup.type}
+              </p>
+              <p>
+                <strong>Keywords:</strong> {meetup.keywords.join(", ")}
+              </p>
+              <p>
+                <strong>Description:</strong> {meetup.description}
+              </p>
+              <p>
+                <strong>Max Attendees:</strong> {meetup.maxAttendees}
+              </p>
+              <p>
+                <strong>Active:</strong> {meetup.activeMeetup ? "Yes" : "No"}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section>
+        <h2>Mis quedadas pasadas</h2>
+        <ul>
+          {pastMeetups.map((meetup) => (
             <li key={meetup.id}>
               <h3>{meetup.title}</h3>
               <p>Date: {meetup.date}</p>
@@ -52,10 +106,7 @@ const pastMeetups = userMeetups.filter(meetup => !meetup.activeMeetup);
             </li>
           ))}
         </ul>
-</section>
-
-
-
+      </section>
     </div>
   );
 };
